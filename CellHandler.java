b27b51f122e1dab577a3916b08df4623e1e2d33f
eudus <eudusb@shaw.ca>
2025-03-04 16:40:36 -0800

    Added basics to Main and CellHandler

---
+++ CellHandler.java
@@ -0,0 +1,27 @@
+package CW_gol;
+
+/**
+ * A class used for handling a thread that manages a block
+ * of cells
+ *
+ * @author Logan White
+ */
+public class CellHandler implements Runnable {
+    /**
+     * Creates a new CellHandler object
+     *
+     * @param cellMatrix A matrix of switches for each cell
+     * @param xMin The start of the range (inclusive) of cells to handle on the x-axis
+     * @param xMax The end of the range (inclusive) of cells to handle on the x-axis
+     * @param yMin The start of the range (inclusive) of cells to handle on the y-axis
+     * @param yMax The end of the range (inclusive) of cells to handle on the y-axis
+     */
+    public CellHandler(boolean[][] cellMatrix, int xMin, int xMax, int yMin, int yMax) {
+        
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
