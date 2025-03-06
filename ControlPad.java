0d168ca63030e6a8ed040ff8d3e529c54b1c632b
eudus <eudusb@shaw.ca>
2025-03-05 17:40:01 -0800

    Added skeletons

---
+++ ControlPad.java
@@ -0,0 +1,9 @@
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+
+public class ControlPad extends JPanel {
+    public ControlPad(ActionListener startCallback, ActionListener pauseCallback,
+        ActionListener resetCallback, ActionListener closeCallback) {
+
+    }
+}
